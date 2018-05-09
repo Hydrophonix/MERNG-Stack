8bc9b9715661414ad01d2703382a48007637b4db
Hydrophonix <hydrophonixfx@gmail.com>
2018-05-09 16:23:24 +0300

    added express and some functionality

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-const { graphql, buildSchema } = require('graphql')
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+const crypto = require('crypto')
 
 const db = {
   users: [
-    { id: '1', email: 'boss@gmail@com', name: 'Boss' },
-    { id: '2', email: 'hugo@gmail.com', name: 'Hugo' }
+    { id: '1', email: 'boss@gmail@com', name: 'Boss', avatarUrl: 'https://gravatar.com/...'},
+    { id: '2', email: 'hugo@gmail@com', name: 'Hugo', avatarUrl: 'https://gravatar.com/...'}
+  ],
+  messages: [
+    {id: '1', userId: '1', body: 'Hello', createdAt: Date.now() },
+    {id: '2', userId: '2', body: 'Hi', createdAt: Date.now() },
+    {id: '3', userId: '1', body: 'Whats up', createdAt: Date.now() }
   ]
+}
+
+class User {
+  constructor (user) {
+    Object.assign(this, user)
+  }
+
+  get messages () {
+    return db.messages.filter(message => message.userId === this.id)
+  }
 }
 
 const schema = buildSchema(`
   type Query {
     users: [User!]!
+    user(id: ID!): User
+    messages: [Message!]!
+  }
+
+  type Mutation {
+    addUser(email: String!, name: String): User
   }
 
   type User {
@@ -17,26 +41,40 @@ const schema = buildSchema(`
     email: String
     name: String
     avatarUrl: String
+    messages: [Message!]!
+  }
+
+  type Message {
+    id: ID!
+    body: String!
+    createdAt: String
   }
 `)
 
 const rootValue = {
-  users: () => db.users
+  users: () => db.users.map(user => new User(user)),
+  user: args => db.users.find(user => user.id === args.id),
+  messages: () => db.messages,
+  addUser: ({ email, name }) => {
+    const user = {
+      id: crypto.randomBytes(10).toString('hex'),
+      email,
+      name
+    }
+
+    db.users.push(user)
+
+    return user
+  }
 }
 
-graphql(
+const app = express()
+const port = 3000
+
+app.use('/graphql', graphqlHTTP({
   schema,
-  `
-    {
-      users {
-        id
-        email
-      }
-    }
-  `,
-  rootValue
-).then(
-  res => console.dir(res, { depth: null })
-).catch(
-  console.error
-)
+  rootValue,
+  graphiql: true
+}))
+
+app.listen(port, () => console.log(`Listening on ${port}`))
